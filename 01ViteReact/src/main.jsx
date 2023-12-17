d1535b8738c00ebce26474bdea347d2aab3ed35f
hshekhar101 <himanshushekhar44578@gmail.com>
2023-12-17 22:17:29 +0530

    custom project is also done

--- 01ViteReact/src/main.jsx
+++ 01ViteReact/src/main.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <>
+    <h1>Using MY APP inside main.js</h1>
+    </>
+  )
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <MyApp />
   </React.StrictMode>,
 )
